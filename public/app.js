adc46894afb611328b1b38c40e61558b38e14267
zy2lfh <70933656+zy2lfh@users.noreply.github.com>
2020-09-07 22:41:41 -0700

    mute audio

--- public/app.js
+++ public/app.js
@@ -186,7 +186,7 @@ async function joinRoomById(roomId) {
 
 async function openUserMedia(e) {
   const stream = await navigator.mediaDevices.getUserMedia(
-      {video: true, audio: true});
+      {video: true, audio: false});
   document.querySelector('#localVideo').srcObject = stream;
   localStream = stream;
   remoteStream = new MediaStream();
